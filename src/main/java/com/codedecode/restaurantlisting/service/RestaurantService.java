46e51854864a57bc486d93ca5843b768e92f9e7a
Mopendra Kumar <mopendra.athe@gmail.com>
2024-05-26 17:23:02 +0530

    first commit
    first commit

---
+++ src/main/java/com/codedecode/restaurantlisting/service/RestaurantService.java
@@ -0,0 +1,40 @@
+package com.codedecode.restaurantlisting.service;
+
+import com.codedecode.restaurantlisting.dto.RestaurantDTO;
+import com.codedecode.restaurantlisting.entity.Restaurant;
+import com.codedecode.restaurantlisting.entity.mapper.RestaurantMapper;
+import com.codedecode.restaurantlisting.repo.RestaurantRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Component
+public class RestaurantService {
+
+    @Autowired
+    RestaurantRepo restaurantRepo;
+
+    public List<RestaurantDTO> findAllRestaurants() {
+        List<Restaurant> restaurants = restaurantRepo.findAll();
+        List<RestaurantDTO> restaurantDTOList = restaurants.stream().map(restaurant -> RestaurantMapper.INSTANCE.mapRestaurantToRestaurantDTO(restaurant)).collect(Collectors.toList());
+        return restaurantDTOList;
+    }
+
+    public RestaurantDTO addRestaurantInDB(RestaurantDTO restaurantDTO) {
+        Restaurant restaurant = restaurantRepo.save(RestaurantMapper.INSTANCE.mapRestaurantDTOToRestaurant(restaurantDTO));
+        return RestaurantMapper.INSTANCE.mapRestaurantToRestaurantDTO(restaurant);
+    }
+
+    public ResponseEntity<RestaurantDTO> fetchRestaurantById(Integer id) {
+        Optional<Restaurant> restaurant = restaurantRepo.findById(id);
+        if (restaurant.isPresent()){
+            return new ResponseEntity<>(RestaurantMapper.INSTANCE.mapRestaurantToRestaurantDTO(restaurant.get()), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(null,HttpStatus.NOT_FOUND);
+    }
+}
